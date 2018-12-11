7af5ee75d76a2f8dbc2829094c5618a2893eae96
annaboyatyuk <annaboyatyuk@gmail.com>
2018-12-10 18:52:14 -0800

    fizz buzz tree complete

--- __test__/binary_trees/fizzBuzzTree.test.js
+++ __test__/binary_trees/fizzBuzzTree.test.js
@@ -16,4 +16,30 @@ describe('fizzBuzztree()', () => {
 
     expect(fizzBuzzTree(BST)).toBe(BST);
   });
+
+
+  it('should change numbers divisible by 3 to Fizz', () => {
+
+    BST.insert(7);
+    BST.insert(6);
+    BST.insert(8);
+    BST.insert(11);
+    BST.insert(2);
+    BST.insert(30);
+    BST.insert(5);
+    
+    /*
+          7
+        6   8
+      2       11
+       5         30 
+    */
+    expect(fizzBuzzTree(BST).root.left.val).toBe('Fizz');
+
+    expect(fizzBuzzTree(BST).root.left.left.right.val).toBe('Buzz');
+
+    expect(fizzBuzzTree(BST).root.right.right.right.val).toBe('FizzBuzz');
+  });
+
+
 });
--- src/binary_trees/fizzBuzzTree.js
+++ src/binary_trees/fizzBuzzTree.js
@@ -1,5 +1,32 @@
 const fizzBuzzTree = (bst) => {
+
+  depth(bst.root);
+  console.log(bst.root.val);
   return bst;
 };
+
+
+function depth(node) {
+
+  if(!node) {
+    return node;
+  }
+
+  if(node.val % 3 === 0 && node.val % 5 === 0) {
+    node.val = 'FizzBuzz';
+  } else if(node.val % 3 === 0) {
+
+    node.val = 'Fizz';
+  } else if(node.val % 5 === 0) {
+    node.val = 'Buzz';
+  }
+
+  depth(node.left);
+  depth(node.right);
+
+
+  return node;
+
+}
 
 export default fizzBuzzTree;
